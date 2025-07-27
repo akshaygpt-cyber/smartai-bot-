a300073353c4b588671665b9b3af9ecde3061b0d
akshaygpt-cyber <akshaypawar111111@gmail.com>
2025-07-27 15:14:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -84,4 +84,4 @@ if __name__ == '__main__':
     if __name__ == '__main__':
     import os
     port = int(os.environ.get("PORT", 5000))  # Render साठी आवश्यक आहे
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port) 
