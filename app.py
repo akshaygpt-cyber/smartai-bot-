7075726f63b2cca453a81d6275c48fde11ec1c0e
akshaygpt-cyber <akshaypawar111111@gmail.com>
2025-07-27 14:32:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -81,5 +81,5 @@ def webhook():
     return jsonify({"status": "ok"})
 
 if __name__ == '__main__':
-    port = int(os.getenv("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get("PORT", 10000))
+app.run(host='0.0.0.0', port=port)
